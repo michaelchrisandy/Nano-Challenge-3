a80ccee79e087206733af7a015a5476feab297be
michaelchrisandy <122039710+michaelchrisandy@users.noreply.github.com>
2024-07-10 13:48:43 +0700

    Initial Commit

---
+++ Nano Challenge 3/Nano_Challenge_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano_Challenge_3App.swift
+//  Nano Challenge 3
+//
+//  Created by Michael Chrisandy on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Nano_Challenge_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
